a5e51ccb8c6b952d931a7d74e33713405462d28b
Aizaz Ata-Husaain Chaudhry <aizazatachaudhry@gmail.com>
2023-01-19 16:08:58 +0000

    removing data entry

--- hospitalisation_plots.R
+++ hospitalisation_plots.R
@@ -2,71 +2,8 @@
 # Hospitalistion data UKHSA 
 
 #Reading in 
-library(readxl)
-library(pacman)
-library(dplyr)
-library(robvis)
-library(stringr)
-library(gt)
-library(gtsummary)
-library(scales)
-library(ggrepel)
-library(mada)
-p_load(tidyverse, knitr, RColorBrewer, kableExtra, ggpubr, ggplot2)
-
-#create data frame for sentinal rate for each season
-
-# 17-18 season
-data_2017 <- read.csv("2017-18_flu_hospital_data.csv")
-week <- data_2017$Week.no
-week <- ifelse(week>=40, week-39, week+13)
-hosp_17_18 <- data_2017$sent_rate
-
-# 18-19 season 
-path_18_19 <- "2018-19_flu_season_data.xlsx"
-df_list_201819 <- list()
-sheet_vector_201819 <- path_18_19 %>% excel_sheets()
-for (i in 1:length(sheet_vector_201819)) {
-  
-  df_list_201819[[i]] <- tibble(read_xlsx(path_18_19, sheet_vector_201819[i], skip=7))
-}
-names(df_list_201819) <- sheet_vector_201819
-hosp_18_19 <- df_list_201819$USISS_Sentinel$`2018-19 Hospital admission (rate)`
-
-# 19-20 season
-
-path_19_20 <- "2019-20_flu_season_data.xlsx"
-df_list_201920 <- list()
-sheet_vector_201920 <- path_19_20 %>% excel_sheets()
-for (i in 1:length(sheet_vector_201920)) {
-  
-  df_list_201920[[i]] <- tibble(read_xlsx(path_19_20, sheet_vector_201920[i], skip=7))
-}
-names(df_list_201920) <- sheet_vector_201920
-hosp_19_20 <- df_list_201920$USISS_Sentinel$`Hospital admission (rate)`
-
-#22-23
-path <- "2022-Influenza_excl.xlsx"
-df_list_2022 <- list()
-#Some chicanery
-sheet_vector_2022 <- path %>% excel_sheets()
-for (i in 1:length(sheet_vector_2022)) {
-  
-  df_list_2022[[i]] <- tibble(read_xlsx(path, sheet_vector_2022[i], skip=7))
-}
-names(df_list_2022) <- sheet_vector_2022
-hosp_22_23 <- df_list_2022$`Figure_35__SARI_Watch-hospital` %>% 
-  filter( df_list_2022$`Figure_35__SARI_Watch-hospital`$`Week number`>39) %>% 
-  pull(3)
-
-nas <- rep(NA, 20)
-hosp_22_23 <- c(hosp_22_23, nas)
-#create one data frame for the weeks and rate per year
-hosp_seasons <- data.frame(week, hosp_17_18, hosp_18_19, hosp_18_19, hosp_22_23) 
-
-print(df_list_2022$`Figure_35__SARI_Watch-hospital`, n = 52)
-
-
+source("source_data_entry.R")
+here::i_am("hospitalisation_plots.R")
 
 #plot all lines on pne plot
 ggplot(hosp_seasons) +
--- swabs.R
+++ swabs.R
@@ -1,69 +1,9 @@
 # Szymon Jakobsze
 # Flu swab data UKHSA & Datamart
 
-library(pacman)
-p_load(tidyverse, here, viridis, hrbrthemes, reshape2, ggpubr, wesanderson, readxl)
-
+source("source_data_entry.R")
 here::i_am("swabs.R")
 
-swabs <- tibble(read_csv(here("allData", "swab", "2014 - 2021 swab data.csv")))
-swabs <- swabs[,-1] %>% select(-year, -week) 
-
-swab_season17_18 <- swabs %>% slice(175:207)
-swab_season18_19 <- swabs %>% slice(227:259)
-swab_season19_20 <- swabs %>% slice(279:311)
-
-swab_season22_23 <- tibble(df_list_2022$`Figure_10__Datamart_-_Flu`) %>% 
-  slice(14:46) 
-
-# Add some supplementary data from the 2023
-path <- "weekly_report_flu_2023.xlsx"
-df_list_2023 <- list()
-
-sheet_vector_2023 <- path %>% excel_sheets()
-
-for (i in 1:length(sheet_vector_2023)) {
-  
-  df_list_2023[[i]] <- tibble(read_xlsx(path, sheet_vector_2023[i], skip=7))
-}
-names(df_list_2023) <- sheet_vector_2023
-
-supplement_data_2023 <- df_list_2023$`Figure_10__Datamart_-_Flu`
-
-swab_season22_23[14,2:5] <- supplement_data_2023[27,2:5]
-
-swab_season22_23$flu_A <- rowSums(swab_season22_23[2:4])
-colnames(swab_season22_23)[5] = 'flu_B'
-
-swab_season22_23 <- swab_season22_23 %>% select ( flu_A,
-                                                  flu_B)
-
-
-
-# Add id number to plot the data
-swab_season17_18$id <- 1:nrow(swab_season17_18)
-swab_season18_19$id <- 1:nrow(swab_season18_19)
-swab_season19_20$id <- 1:nrow(swab_season19_20)
-swab_season22_23$id <- 1:nrow(swab_season22_23)
-
-
-# Create data frames for 2 separate flu types
-typeA1 <- tibble(id = swab_season17_18$id,
-                 '17-18' = swab_season17_18$flu_A,
-                 '18-19' = swab_season18_19$flu_A,
-                 '19-20' = swab_season19_20$flu_A,
-                 '22-23' = swab_season22_23$flu_A)
-
-typeA <- melt(typeA1 ,  id.vars = 'id', variable.name = 'series')
-
-typeB1 <- tibble(id = swab_season17_18$id,
-                 '17-18' = swab_season17_18$flu_B,
-                 '18-19' = swab_season18_19$flu_B,
-                 '19-20' = swab_season19_20$flu_B,
-                 '22-23' = swab_season22_23$flu_B)
-
-typeB <- melt(typeB1 ,  id.vars = 'id', variable.name = 'series')
-#Aizaz stopped copying here for source_data_entry
 # Create initial plots
 plotA <- ggplot(typeA, aes(id, value)) +
   geom_line(lwd = 1.5, aes(colour = series)) +
