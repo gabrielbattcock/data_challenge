0ff376e244a4914a78a8bd0a4a6c2045f7da1331
rizlasilver210 <117362676+rizlasilver210@users.noreply.github.com>
2023-01-24 17:30:45 +0000

    Fixed the bug in the plot

--- vacc_plot.R
+++ vacc_plot.R
@@ -1,13 +1,3 @@
-# Transforming the vaccination datainto a plottable format
-
-vacc_rate <- select(allvac, 
-                    'Year' = year,
-                    'Over 65 year' = over65_pct,
-                    '50-65 with clinical risk' = atrisk_pct,
-                    'Pregnant women' = pregnt_pct,
-                    '2-3 year olds' = y2and3_pct) %>% pivot_longer(-Year) %>% arrange(., Year)
-
-
 # Create a plot for vaccination rate stratified by risk groups
 
 vacc_plot <- ggplot(vacc_rate, aes(fill=name, y=value, x=Year)) + 
@@ -19,7 +9,7 @@ vacc_plot <- ggplot(vacc_rate, aes(fill=name, y=value, x=Year)) +
   theme(panel.border = element_rect(color = "dark grey",
                                     fill = NA,
                                     size = 0.1),
-        plot.margin = margin(t = 5, r = 5, b = 7, l = 5),
+        plot.margin = ggplot2::margin(t = 5, r = 5, b = 7, l = 5),
         panel.grid.major.y = element_line(color = "red",
                                           size = 0.1,
                                           linetype = 2),
@@ -31,5 +21,3 @@ vacc_plot <- ggplot(vacc_rate, aes(fill=name, y=value, x=Year)) +
   scale_y_continuous(breaks = seq(0,100,20),
                      minor_breaks = seq(0,100,5)) +
   coord_cartesian(ylim = c(0,90), expand = T) 
-
-
