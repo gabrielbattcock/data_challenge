b30e2e03b77b7e33e6805cb89474dcc829055c38
Gabriel Battcock <gabrielbattcock@gmail.com>
2023-01-27 13:56:35 +0000

    log graph for every year with regression line

--- log_graph_trial.R
+++ log_graph_trial.R
@@ -25,10 +25,10 @@ swab_df <- swabs %>% slice(279:311)
 swab_df$total = swab_df$flu_A+swab_df$flu_B
 swab <- (swab_df$total/56000000)*100000
 week <- seq(1:33)
-season_201920 <- data.frame( week,hospital$`2018-19 Hospital admission (rate)`, gp, swab)
+season_201920 <- data.frame( week,hospital$`Hospital admission (rate)`, gp, swab)
 names(season_201920) <- c("week", "hospital", "gp", "swab")
 
-log_hosp <- log10(hospital$`2018-19 Hospital admission (rate)`)
+log_hosp <- log10(hospital$`Hospital admission (rate)`)
 log_week <- log10(week)
 log_gp <- log10(gp)
 log_swab <- log10(swab)
@@ -36,7 +36,7 @@ log_season <- data.frame(week, log_hosp = log_hosp, log_gp, log_swab)
 log_season_subset <- data.frame(week = week[5:12], hosp = log_season$log_hosp[5:12], 
                                 gp = log_season$log_gp[5:12], swab = log_season$log_swab[5:12])
 
-#creat linear models for when 
+#create linear models for when 
 lm_hosp <- lm(data = log_season_subset,formula = hosp~week)
 hosp_predict <- data.frame(hosp_line = predict(lm_hosp, log_season_subset), week = log_season_subset$week)
 lm_gp <- lm(data = log_season_subset,formula = gp~week)
@@ -48,21 +48,17 @@ summary(lm_hosp)
 summary(lm_swab)
 
 #check whether the R_0 rates are similar
-confint(lm_gp, level = 0.95)
-
-10**0.08166553
-10**(0.102)
 10**(0.148)
 
 
-log_hosp_plot <- ggplot(log_season) +
+log_hosp_plot1920 <- ggplot(log_season) +
   theme_ipsum() +
   geom_line(lwd = 1.5, aes(week, log_hosp, color = 'Hospital')) +
   geom_line(lwd = 1.5, aes(week, log_gp, color = 'GP')) +
   geom_line(lwd = 1.5, aes(week, log_swab, color = 'Swab')) +
   geom_line(data = hosp_predict, aes(week, hosp_line)) +
   geom_line(data = gp_predict, aes(week, gp_line)) + 
-  #geom_line(data = swab_predict, aes(gp_line, week))+
+  geom_line(data = swab_predict, aes(week, gp_line))+
   # guides(color = guide_legend("Data source")) +
   # ylab("Log rate") +
   # # xlim(-12, 20)+
@@ -78,13 +74,139 @@ log_hosp_plot <- ggplot(log_season) +
   theme(panel.border = element_rect(color = "dark grey",
                                     fill = NA,
                                     size = 0.1)) +
-  coord_cartesian(ylim = c(-1, 25), expand = FALSE) +
+  coord_cartesian(ylim = c(-1, 2), expand = FALSE) +
   scale_color_manual('Season', values= wes_palette("Moonrise1", n = 3))
 
-log_hosp_plot 
+log_hosp_plot_1920
 # geom_smooth(data = log_season_subset, formula = hosp~week, method = 'lm')
-#normalising it so all the peaks are the same heigh (swab data is in number not rate)
-# hospital_norm <- hospital/mean(hospital), rm.na = T)
 
+
+#2018-19 data
+
+hospital18 <- df_list_201819$USISS_Sentinel[4]
+gp18 =  as.numeric(df_list_201819$RCGP$...3[2:34])
+swab_df <- swab_season18_19
+swab_df$total = swab_df$flu_A+swab_df$flu_B
+swab18 <- (swab_df$total/56000000)*100000
+season_201819 <- data.frame( week,hospital18, gp18, swab18)
+names(season_201819) <- c("week", "hospital", "gp", "swab")
+
+
+log_hosp <- log10(hospital18$`2018-19 Hospital admission (rate)`)
+log_week <- log10(week)
+log_gp <- log10(gp18)
+log_swab <- log10(swab18)
+log_season <- data.frame(week, log_hosp = log_hosp, log_gp, log_swab)
+log_season_subset <- data.frame(week = week[7:17], hosp = log_season$log_hosp[7:17], 
+                                gp = log_season$log_gp[7:17], swab = log_season$log_swab[7:17])
+
+#create linear models for when 
+lm_hosp <- lm(data = log_season_subset,formula = hosp~week)
+hosp_predict <- data.frame(hosp_line = predict(lm_hosp, log_season_subset), week = log_season_subset$week)
+lm_gp <- lm(data = log_season_subset,formula = gp~week)
+gp_predict <- data.frame(gp_line = predict(lm_gp, log_season_subset), week = log_season_subset$week)
+lm_swab <- lm(data = log_season_subset,formula = swab~week)
+swab_predict <- data.frame(gp_line = predict(lm_swab, log_season_subset), week = log_season_subset$week)
+summary(lm_gp)
+summary(lm_hosp)
+summary(lm_swab)
+
+#check whether the R_0 rates are similar
+
+
+
+log_hosp_plot1819 <- ggplot(log_season) +
+  theme_ipsum() +
+  geom_line(lwd = 1.5, aes(week, log_hosp, color = 'Hospital')) +
+  geom_line(lwd = 1.5, aes(week, log_gp, color = 'GP')) +
+  geom_line(lwd = 1.5, aes(week, log_swab, color = 'Swab')) +
+  geom_line(data = hosp_predict, aes(week, hosp_line)) +
+  geom_line(data = gp_predict, aes(week, gp_line)) + 
+  geom_line(data = swab_predict, aes(week, gp_line))+
+  # guides(color = guide_legend("Data source")) +
+  # ylab("Log rate") +
+  # # xlim(-12, 20)+
+  ggtitle("Log tranformed influenza cases 2018-19 \n per different data sources") +
+  scale_x_continuous(breaks = seq(0, 34, 2),
+                     minor_breaks = seq(0, 34, 1),
+                     labels = c("40", "42", "44",
+                                "46", "48",
+                                "50", "52", "2",
+                                "4", "6", "8", "10",
+                                "12","14","16",
+                                "18", "20", "22")) +
+  theme(panel.border = element_rect(color = "dark grey",
+                                    fill = NA,
+                                    size = 0.1)) +
+  coord_cartesian(ylim = c(-1, 2), expand = FALSE) +
+  scale_color_manual('Season', values= wes_palette("Moonrise1", n = 3))
+
+log_hosp_plot1819
+
+#2017-18 data
+
+hospital17 <- data_2017$sent_rate
+gp17 =  as.numeric(primary_care_201718$`GP ILI consulation rates (all ages)`[2:34])
+swab_df <- swab_season17_18
+swab_df$total = swab_df$flu_A+swab_df$flu_B
+swab17 <- (swab_df$total/56000000)*100000
+week <- seq(1:33)
+season_201718 <- data.frame( week,hospital17, gp17, swab17)
+names(season_201718) <- c("week", "hospital", "gp", "swab")
+
+
+log_hosp <- log10(hospital17)
+log_week <- log10(week)
+log_gp <- log10(gp17)
+log_swab <- log10(swab17)
+log_season <- data.frame(week, log_hosp = log_hosp, log_gp, log_swab)
+log_season_subset <- data.frame(week = week[7:15], hosp = log_season$log_hosp[7:15], 
+                                gp = log_season$log_gp[7:15], swab = log_season$log_swab[7:15])
+
+#create linear models for when 
+lm_hosp <- lm(data = log_season_subset,formula = hosp~week)
+hosp_predict <- data.frame(hosp_line = predict(lm_hosp, log_season_subset), week = log_season_subset$week)
+lm_gp <- lm(data = log_season_subset,formula = gp~week)
+gp_predict <- data.frame(gp_line = predict(lm_gp, log_season_subset), week = log_season_subset$week)
+lm_swab <- lm(data = log_season_subset,formula = swab~week)
+swab_predict <- data.frame(gp_line = predict(lm_swab, log_season_subset), week = log_season_subset$week)
+summary(lm_gp)
+summary(lm_hosp)
+summary(lm_swab)
+
+#check whether the R_0 rates are similar
+
+
+
+log_hosp_plot1718 <- ggplot(log_season) +
+  theme_ipsum() +
+  geom_line(lwd = 1.5, aes(week, log_hosp, color = 'Hospital')) +
+  geom_line(lwd = 1.5, aes(week, log_gp, color = 'GP')) +
+  geom_line(lwd = 1.5, aes(week, log_swab, color = 'Swab')) +
+  geom_line(data = hosp_predict, aes(week, hosp_line)) +
+  geom_line(data = gp_predict, aes(week, gp_line)) + 
+  geom_line(data = swab_predict, aes(week, gp_line))+
+  # guides(color = guide_legend("Data source")) +
+  # ylab("Log rate") +
+  # # xlim(-12, 20)+
+  ggtitle("Log tranformed influenza cases 2017-18 \n per different data sources") +
+  scale_x_continuous(breaks = seq(0, 34, 2),
+                     minor_breaks = seq(0, 34, 1),
+                     labels = c("40", "42", "44",
+                                "46", "48",
+                                "50", "52", "2",
+                                "4", "6", "8", "10",
+                                "12","14","16",
+                                "18", "20", "22")) +
+  theme(panel.border = element_rect(color = "dark grey",
+                                    fill = NA,
+                                    size = 0.1)) +
+  coord_cartesian(ylim = c(-1, 2), expand = FALSE) +
+  scale_color_manual('Season', values= wes_palette("Moonrise1", n = 3))
+
+log_hosp_plot1718
+
+
+#
 
 
